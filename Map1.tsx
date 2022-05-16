5a504e96d9319126fc0ef46ad30d971281facfe8
Rayan <rchouchane@iut.univ-paris8.fr>
2022-05-16 09:43:56 +0200

    Ajout test de modif

---
+++ Map1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Map" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="../Tileset Map/FreeCuteTileset/ExampleTiles.png" width="320" height="192"/>
+</tileset>
