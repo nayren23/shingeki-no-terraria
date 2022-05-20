09112e392450253108047b46913a9b65c69d0f02
Rayan <rchouchane@iut.univ-paris8.fr>
2022-05-20 11:03:02 +0200

    patch pour affichage de la map avec suppression création imagView

--- src/jeu/vue/TerrainVue.java
+++ src/jeu/vue/TerrainVue.java
@@ -19,62 +19,62 @@ public class TerrainVue {
 	public void dessinerTerrain () {
 		tuilesFond.getChildren().clear();
 		for(int cases = 0; cases < terrain.getTerrain().length ; cases++) {
-			ImageView img;
+			ImageView img = new ImageView();
 			switch(terrain.getTerrain()[cases]) {
 
 			case 58 :
-				img = new ImageView(new Image("jeu/image/terre.png"));
+				img.setImage(new Image("jeu/image/terre.png"));
 				break;
 			case 0 :
-				img = new ImageView(new Image("jeu/image/ciel.png"));
+				img.setImage(new Image("jeu/image/ciel.png"));
 				break;
 				
 			case 1 :
-				img = new ImageView(new Image("jeu/image/1.png"));
+				img.setImage(new Image("jeu/image/1.png"));
 				break;
 				
 			case 2 :
-				img = new ImageView(new Image("jeu/image/2.png"));
+				img.setImage(new Image("jeu/image/2.png"));
 				break;
 				
 			case 3 :
-				img = new ImageView(new Image("jeu/image/3.png"));
+				img.setImage(new Image("jeu/image/3.png"));
 				break;
 				
 			case 4 :
-				img = new ImageView(new Image("jeu/image/4.png"));
+				img.setImage(new Image("jeu/image/4.png"));
 				break;
 				
 			case 5 :
-				img = new ImageView(new Image("jeu/image/5.png"));
+				img.setImage(new Image("jeu/image/5.png"));
 				break;
 				
 			case 6 :
-				img = new ImageView(new Image("jeu/image/6.png"));
+				img.setImage(new Image("jeu/image/6.png"));
 				break;
 				
 			case 7 :
-				img = new ImageView(new Image("jeu/image/7.png"));
+				img.setImage(new Image("jeu/image/7.png"));
 				break;
 				
 			case 8 :
-				img = new ImageView(new Image("jeu/image/8.png"));
+				img.setImage(new Image("jeu/image/8.png"));
 				break;
 
 			case 9 :
-				img = new ImageView(new Image("jeu/image/9.png"));
+				img.setImage(new Image("jeu/image/9.png"));
 				break;
 				
 			case 10 :
-				img = new ImageView(new Image("jeu/image/10.png"));
+				img.setImage(new Image("jeu/image/10.png"));
 				break;
 				
 			case 11 :
-				img = new ImageView(new Image("jeu/image/11.png"));
+				img.setImage(new Image("jeu/image/11.png"));
 				break;
 				
 			case 12 :
-				img = new ImageView(new Image("jeu/image/12.png"));
+				img.setImage(new Image("jeu/image/12.png"));
 				break;
 				
 			default : 
