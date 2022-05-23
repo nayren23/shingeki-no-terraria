82edda7db08f0200c687d47c9deeb4e27fc33af9
Rayan <rchouchane@iut.univ-paris8.fr>
2022-05-23 14:30:27 +0200

    Création du tableua pour éviter la création des images à chaque fois

--- src/jeu/vue/HerosVieVue.java
+++ src/jeu/vue/HerosVieVue.java
@@ -1,9 +1,10 @@
 package jeu.vue;
 
+import java.util.ArrayList;
+
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
 import jeu.model.Personnage;
 
 public class HerosVieVue extends Pane  {
@@ -20,62 +21,63 @@ public class HerosVieVue extends Pane  {
 	}
 
 
-	public void clearPanVieHero() {  // pour éviter que  les coeur s'affiche en meme temp
-		this.PanePrincipale.getChildren().clear();
+	//Création d'un tableua pour stocker les images pour éviter leur création à chaque fois
+	public final Image [] tableauImageCoeur() {
+		Image tableauImage [] = new Image[10] ;
+		tableauImage[0] = new Image("jeu/image/coeur/coeur10.png");
+		tableauImage[1] = new Image("jeu/image/coeur/coeur9.png");
+		tableauImage[2] = new Image("jeu/image/coeur/coeur8.png");
+		tableauImage[3] = new Image("jeu/image/coeur/coeur7.png");
+		tableauImage[4] = new Image("jeu/image/coeur/coeur6.png");
+		tableauImage[5] = new Image("jeu/image/coeur/coeur5.png");
+		tableauImage[6] = new Image("jeu/image/coeur/coeur4.png");
+		tableauImage[7] = new Image("jeu/image/coeur/coeur3.png");
+		tableauImage[8] = new Image("jeu/image/coeur/coeur2.png");
+		tableauImage[9] = new Image("jeu/image/coeur/coeur1.png");
+		return tableauImage;
 	}
+
 		
 	public void affichageVie(int pv) {
-
-		Image imageduCoeur10 =new Image("jeu/image/coeur/coeur10.png");
-		Image imageduCoeur9 = new Image("jeu/image/coeur/coeur9.png");
-		Image imageduCoeur8= new Image("jeu/image/coeur/coeur8.png");
-		Image imageduCoeur7 = new Image("jeu/image/coeur/coeur7.png");
-		Image imageduCoeur6 = new Image("jeu/image/coeur/coeur6.png");
-		Image imageduCoeur5 = new Image("jeu/image/coeur/coeur5.png");
-		Image imageduCoeur4 = new Image("jeu/image/coeur/coeur4.png");
-		Image imageduCoeur3 = new Image("jeu/image/coeur/coeur3.png");
-		Image imageduCoeur2 = new Image("jeu/image/coeur/coeur2.png");
-		Image imageduCoeur1 = new Image("jeu/image/coeur/coeur1.png");
-
 
 		switch(hero.PvProperty().getValue()) {
 
 		case 0 :
-			imageCoeur.setImage(imageduCoeur10);
+			imageCoeur.setImage(tableauImageCoeur()[0]);
 			break;
 
 		case 1 :
-			imageCoeur.setImage(imageduCoeur9);
+			imageCoeur.setImage(tableauImageCoeur()[1]);
 			break;
 
 		case 2 :
-			imageCoeur.setImage(imageduCoeur8);
+			imageCoeur.setImage(tableauImageCoeur()[2]);
 			break;
 
 		case 3 :
-			imageCoeur.setImage(imageduCoeur7);
+			imageCoeur.setImage(tableauImageCoeur()[3]);
 			break;
 		case 4 :
-			imageCoeur.setImage(imageduCoeur6);
+			imageCoeur.setImage(tableauImageCoeur()[4]);
 			break;
 
 		case 5 :
-			imageCoeur.setImage(imageduCoeur5);
+			imageCoeur.setImage(tableauImageCoeur()[5]);
 			break;
 
 		case 6 :
-			imageCoeur.setImage(imageduCoeur4);
+			imageCoeur.setImage(tableauImageCoeur()[6]);
 			break;
 
 		case 7 :
-			imageCoeur.setImage(imageduCoeur3);
+			imageCoeur.setImage(tableauImageCoeur()[7]);
 			break;
 		case 8 :
-			imageCoeur.setImage(imageduCoeur2);
+			imageCoeur.setImage(tableauImageCoeur()[8]);
 			break;
 		case 9 :
 			System.out.println(imageCoeur);
-			imageCoeur.setImage(imageduCoeur1);
+			imageCoeur.setImage(tableauImageCoeur()[9]);
 			break;
 
 		default : 
