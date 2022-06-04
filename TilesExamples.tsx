7b88af2e3505aea83c5e851bdb8aafd6089be324
Yassine <yhamidi@iut.univ-paris8.fr>
2022-06-04 16:06:54 +0200

    Merge branch 'develop' into feature/saut

---
+++ TilesExamples.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="TilesExamples" tilewidth="16" tileheight="16" tilecount="390" columns="26">
+ <image source="../Tileset Map/Foreground/TilesExamples.png" width="416" height="240"/>
+</tileset>
