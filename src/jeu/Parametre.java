881c610b3361ab06baab8ea5a5d6a4619802319f
Rayan <rchouchane@iut.univ-paris8.fr>
2022-06-09 19:38:45 +0200

    range semi potable mais petit soucis avec la range a gauche

--- src/jeu/Parametre.java
+++ src/jeu/Parametre.java
@@ -1,67 +1,89 @@
 package jeu;
 
 public class Parametre {
-	
-		public static enum DIRECTION {
-			RIGHT, LEFT, BOTTOM, TOP, SPACE;
-		} 
-		
-		//Ici c'est quand on casse le bloc il sera remplacer par le loc choisit en bas
+
+	public static enum DIRECTION {
+		RIGHT, LEFT, BOTTOM, TOP, SPACE;
+	} 
+
+	//Ici c'est quand on casse le bloc il sera remplacer par le loc choisit en bas
 
 
-        public static int tailleTuile = 32;
-        
-        public static int longueurTerrain = 40;
-        
-        public static int vitessePersonnage = 3;
-        
-        public static int hauteurSautPersonnage = 7;
-        
-        public static int forceGravite = 3;
-        
+	public static int tailleTuile = 32;
 
-        
-        public static int largeurPersonnage = 20;
-        
-        public static int hauteurPersonnage = 25;
-        
-        public static int diffBlocDessu = 32;
-        
-        
-        public static int changementDuBlocCasser = 0; //a changer
+	public static int longueurTerrain = 40;
+
+	public static int vitessePersonnage = 3;
+
+	public static int hauteurSautPersonnage = 7;
+
+	public static int forceGravite = 3;
 
 
-        public static final int blocTerre = 2; // Bloc de terre final car pas senser chanegr
-        
-        public static final int blocCharbon = 3; // Bloc de terre final car pas senser chanegr
-        
-        public static final int blocGaz = 4; // Bloc de terre final car pas senser chanegr
-	
-        public static final int blocFer = 5; // Bloc de terre final car pas senser chanegr
 
-        
-        ///////////////////////////////////////////
-     //   Parametre Titan
-        //recharge du titan initialiser a 0
-        public  static  int rechargeAttaqueTitans = 0; 
-        
-        public  static  int rechargeAttaqueTitansTemps = 60; 
+	public static int largeurPersonnage = 20;
 
-        public static int porteeCoupDuTitanX = 50;
+	public static int hauteurPersonnage = 25;
 
-        public static int porteeCoupDuTitanY = 10;
+	public static int diffBlocDessu = 32;
 
-        public 	static	int vitesseTitan =2;
-        
-        public static int nbTitansGenerer = 6;
-        ///////////////////////////////////////////
 
-        public static int coordonneeMinTerrainTitanGauche =320;
-        
-        public static int coordonneeMaxTerrainTitanDroite =1052;
+	public static int changementDuBlocCasser = 0; //a changer
 
-        public static int coordonneeTerrainTitanY =450;
 
-        //---------------------------------------------------------------------------------------//
+	public static final int blocTerre = 2; // Bloc de terre final car pas senser chanegr
+
+	public static final int blocCharbon = 3; // Bloc de terre final car pas senser chanegr
+
+	public static final int blocGaz = 4; // Bloc de terre final car pas senser chanegr
+
+	public static final int blocFer = 5; // Bloc de terre final car pas senser chanegr
+
+
+	///////////////////////////////////////////
+	//   Parametre Titan
+	//recharge du titan initialiser a 0
+	public  static  int rechargeAttaqueTitans = 0; 
+
+	public  static  int rechargeAttaqueTitansTemps = 60; 
+
+	public static int porteeCoupDuTitanX = 50;
+
+	public static int porteeCoupDuTitanY = 10;
+
+	public 	static	int vitesseTitan =2;
+
+	public static int nbTitansGenerer = 6;
+	///////////////////////////////////////////
+
+	public static int coordonneeMinTerrainTitanGauche =320;
+
+	public static int coordonneeMaxTerrainTitanDroite =1052;
+
+	public static int coordonneeTerrainTitanY =450;
+
+	//---------------------------------------------------------------------------------------//
+
+	public static boolean rangePourCasserBloc (int coordonneeJoueurX, int coordonneeJoueurY , int positionBlocX, int positionBlocY ,int rangeShouaiter ) {
+
+		boolean peutCreuser = false;
+
+		if( (coordonneeJoueurX -rangeShouaiter <= positionBlocX  &&  coordonneeJoueurX + rangeShouaiter >=positionBlocX )
+				&&  (coordonneeJoueurY -rangeShouaiter <= positionBlocY &&  coordonneeJoueurY + rangeShouaiter >=positionBlocY) ) {
+			peutCreuser = true;
+			System.out.println("\n tu peux  creuser frerot !!!");
+
+		}
+		else
+			System.out.println("\n tu peux pas creuser frerot !!!");
+		return peutCreuser;
+	}
+
+
+
+
+
+
+
 
 }
--- src/jeu/model/inventaire/outil/Outil.java
+++ src/jeu/model/inventaire/outil/Outil.java
@@ -15,7 +15,7 @@ public abstract class Outil extends Objet{
 
 	public Outil(int id, String nom, Environnement env) {
 		super(id);
-		this.durabilite = 10;
+		this.durabilite = 1000;
 		this.degats = 2;
 		this.nom=nom;
 		this.env = env;
